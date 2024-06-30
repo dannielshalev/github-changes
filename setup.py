7b880e889d1276f6d7e230e5d2b55fbd1a2a9369
danniel <danniel.s@uveye.com>
2024-06-30 20:49:35 +0300

    fix requirements.txt path

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ requirements = parse_requirements(os.path.join(os.path.dirname(__file__), 'requi
 
 setup(
     name='github_changes',
-    version='1.0.0',
+    version='1.0.2',
     author='danniel',
     author_email='danniel.shalev@gmail.com',
     description='A github file changes module',
